035c08941a17509d80ede5a3f6ba3c8ee3fe075f
arantxavalenciaa <valenciarantxa@gmail.com>
2025-04-17 23:56:33 -0600

    Add react files

---
+++ mini_store copy/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HorizontalBarChart from './horizontalBar';
+
+
+function App() {
+  return (
+    <div className = "App" >
+      <h1>Top 10 best products in 2003</h1>
+      <div>
+        <HorizontalBarChart/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
